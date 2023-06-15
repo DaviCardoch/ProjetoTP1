6d5cf4e9328b19837fa08a1bc107f26d87d59ba7
carlos <carloseduardoarnaud@gmail.com>
2023-06-15 18:13:36 -0300

    first commit

---
+++ Controladoras.h
@@ -0,0 +1,12 @@
+#ifndef CONTROLADORAS_H_INCLUDED
+#define CONTROLADORAS_H_INCLUDED
+
+#include "Interfaces.h"
+#include "Dominios.h"
+
+#include <stdexcept>
+#include <iostream>
+
+using namespace std;
+
+#endif // CONTROLADORAS_H_INCLUDED
---
+++ Interfaces.h
@@ -0,0 +1,10 @@
+#ifndef INTERFACES_H_INCLUDED
+#define INTERFACES_H_INCLUDED
+
+#include "dominios.h"
+
+#include <stdexcept>
+
+using namespace std;
+
+#endif // INTERFACES_H_INCLUDED
