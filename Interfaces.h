4609e147585d9261cf006cfd5421a98603ccd282
Marcos Paulo de Oliveira <107584114+Mp0liveira@users.noreply.github.com>
2023-06-20 19:04:16 -0300

    Update Interfaces.h

--- Interfaces.h
+++ Interfaces.h
@@ -1,15 +1,54 @@
 #ifndef INTERFACES_H_INCLUDED
 #define INTERFACES_H_INCLUDED
 
-#include "dominios.h"
-
+#include "Dominios.h"
+#include "Entidades.h"
 #include <stdexcept>
 
-using namespace std;
+class IAAutenticacao {
+    public:
+        virtual bool autenticar(Matricula*) = 0;
+        virtual void setCntrISAutenticacao(ISAutenticacao*) = 0;
+        virtual ~IAAutenticacao(){}
+};
 
 class IADesenvolvedor {
-public:
-    bool cadastrar();
-    virtual bool executar(Matricula) = 0;
-}
+    public:
+        virtual void executar(Matricula) = 0;
+        virtual void cadastrar() = 0;
+        virtual void setCntrISDesenvolvedor(ISDesenvolvedor*) = 0;
+        virtual void ~IADesenvolvedor(){}
+};
+
+class IATeste {
+    public:
+        virtual void executar(Matricula) = 0;
+        virtual void setCtrISTeste(ISTeste*) = 0;
+        virtual ~IATeste(){}
+};
+
+class ISAutenticacao {
+    public:
+        virtual bool autenticar(const Matricula&, const Senha&) = 0;
+        virtual ~ISAutenticacao(){}
+};
+
+class ISDesenvolvedor {
+    public:
+        virtual bool cadastrar(const Desenvolvedor&) = 0;
+        virtual bool visualizar(const Desenvolvedor&) = 0;
+        virtual bool editar(const Desenvolvedor&) = 0;
+        virtual bool descadastrar(const Desenvolvedor&) = 0;
+        virtual ~ISDesenvolvedor(){}
+};
+
+class ISTeste {
+    public:
+        virtual bool cadastrar(const Teste&) = 0;
+        virtual bool visualizar(const Teste&) = 0;
+        virtual bool editar(const Teste&) = 0;
+        virtual bool descadastrar(const Teste&) = 0;
+        virtual ~ISTeste(){}
+};
+
 #endif // INTERFACES_H_INCLUDED
